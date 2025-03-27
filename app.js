9ac12c06a12e46ee658ff805dfcf458caaa528ec
Brendah Edward <edwardbrendah28@gmail.com>
2025-03-27 07:19:51 +0300

    Fetches expenses from the server and displays

--- app.js
+++ app.js
@@ -8,6 +8,36 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#budgetDisplay").textContent = budget;
   })
 
+  fetch("http://localhost:3000/expenses")
+  .then(response => response.json())
+  .then(data => {
+    expense = data;
+    renderExpenses();
+  })
+
+  function renderExpenses() {
+    const expenseList = document.querySelector("#expenseList");
+    expenseList.innerHTML = "";
+    let totalExpenses = 0;
+
+    expenses.forEach(expense => {
+      totalExpenses += expense.amount;
+      expenseList.innerHTML+=  `
+      <tr>
+                  <td>${expense.name}</td>
+                  <td>${expense.category}</td>
+                  <td>${expense.amount}</td>
+                  <td>
+                      <button class="btn-delete" data-id="${expense.id}">Delete</button>
+                  </td>
+              </tr>
+              `;
+    });
+
+    document.querySelector("#totalExpenses").textContent = totalExpenses;
+
+  }
+
 });
 
 
